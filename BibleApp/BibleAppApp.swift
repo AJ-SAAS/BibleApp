32132d10572647963b068aa51f0d9f77a1b9f0f8
Ajay Sharma <gridking111@gmail.com>
2025-08-04 19:56:56 +0300

    Initial Commit

---
+++ BibleApp/BibleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleAppApp.swift
+//  BibleApp
+//
+//  Created by Work Laptop on 04/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct BibleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BibleAppTests/BibleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BibleAppTests.swift
+//  BibleAppTests
+//
+//  Created by Work Laptop on 04/08/2025.
+//
+
+import Testing
+@testable import BibleApp
+
+struct BibleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
